830c7afb9f8abb639e2abdbde296b6df9c027de5
Sissy Eva <sissyeva99@gmail.com>
2025-05-04 14:34:30 +0300

    release v2.1

--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.svg?react' {
-  import { ReactElement, SVGProps } from 'react'
-  const content: (props: SVGProps<SVGElement>) => ReactElement
-  export default content
-}
