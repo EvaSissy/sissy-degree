4cb7c494dfe2200f68aad47aad0ebef8c2bd015c
Sissy Eva <sissyeva99@gmail.com>
2025-05-04 15:09:27 +0300

    release v2.2

--- src/ui/diploma/config/pictures.ts
+++
@@ -1,9 +0,0 @@
-import bg1Src from '@/assets/bg-1.png'
-import bg2Src from '@/assets/bg-2.png'
-import bg3Src from '@/assets/bg-3.png'
-import bg4Src from '@/assets/bg-4.png'
-import bg5Src from '@/assets/bg-5.png'
-import bg6Src from '@/assets/bg-6.png'
-import bg7Src from '@/assets/bg-7.png'
-
-export const pictures = [bg1Src, bg2Src, bg3Src, bg4Src, bg5Src, bg6Src, bg7Src]
